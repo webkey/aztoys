9c9d1ce4b08e29f3d536bea3c83b18362fbf57aa
webkey <webkey_i@ukr.net>
2016-08-01 20:33:06 +0300

    of_01_20160801_2032

--- js/functions.js
+++ js/functions.js
@@ -130,6 +130,7 @@ function equalHeightInit(){
 				parent: $products,
 				resize: true
 			});
+			filtersInit();
 		});
 	}
 }
@@ -138,7 +139,7 @@ function equalHeightInit(){
 /*products behavior*/
 function productsBehavior() {
 	$('.products__item, .previews__item').mouseenter(function () {
-		$(this).closest('.main').css('z-index', '99');
+		$(this).closest('.main').css('z-index', '9');
 	}).mouseleave(function () {
 		$(this).closest('.main').css('z-index', '4');
 	});
@@ -279,6 +280,50 @@ function footerBottom(){
 	}
 }
 /* footer at bottom end */
+
+/*filters*/
+function filtersInit() {
+	// external js: isotope.pkgd.js
+
+	// init Isotope
+	var $grid = $('.products').isotope({
+		itemSelector: '.products__item',
+		// layoutMode: 'fitRows',
+		percentPosition: true
+	});
+
+	// filter functions
+	var filterFns = {
+		// show if number is greater than 50
+		numberGreaterThan50: function() {
+			var number = $(this).find('.number').text();
+			return parseInt( number, 10 ) > 50;
+		},
+		// show if name ends with -ium
+		ium: function() {
+			var name = $(this).find('.name').text();
+			return name.match( /ium$/ );
+		}
+	};
+
+	// bind filter button click
+	$('.filters-button-group').on( 'click', 'button', function() {
+		var filterValue = $( this ).attr('data-filter');
+		// use filterFn if matches value
+		filterValue = filterFns[ filterValue ] || filterValue;
+		$grid.isotope({ filter: filterValue });
+	});
+
+	// change is-checked class on buttons
+	$('.button-group').each( function( i, buttonGroup ) {
+		var $buttonGroup = $( buttonGroup );
+		$buttonGroup.on( 'click', 'button', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$( this ).addClass('is-checked');
+		});
+	});
+}
+/*filters end*/
 
 /** ready/load/resize document **/
 
